22626c7aaa3080d0fd18ad99e8304218082dadf7
bitcamp <bitcamp@DESKTOP-J4V03M8>
2021-08-18 20:49:49 +0900

    first commit

---
+++ src/ch13_1_collections/ArrayListExample.java
@@ -0,0 +1,41 @@
+package ch13_1_collections;
+
+import java.util.*;
+
+public class ArrayListExample {
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		
+		list.add("Java");        // String °´Ã¼¸¦ ÀúÀå
+		list.add("JDBC");
+		list.add("Servlet/JSP");
+		list.add(2, "DateBase");
+		list.add("iBATIS");
+		
+		int size = list.size();   // ÀúÀåµÈ ÃÑ °´Ã¼ ¼ö ¾ò±â
+		System.out.println("ÃÑ °´Ã¼¼ö: " + size);
+		System.out.println();
+		
+		String skill = list.get(2);  //2¹ø ÀÎµ¦½ºÀÇ °´Ã¼ ¾ò±â
+		System.out.println("2:" + skill);
+		System.out.println();
+		
+		for(int i = 0; i<list.size(); i++) { // ÀúÀåµÈ ÃÑ °´Ã¼ ¼ö¸¸Å­ ·çÇÎ
+			String str = list.get(i);
+			System.out.println(i + " : " + str);
+		}
+		System.out.println();
+		
+		list.remove(2);              // 2¹ø ÀÎµ¦½º °´Ã¼(database) »èÁ¦
+		list.remove(2);				//2¹ø ÀÎµ¦½º °´Ã¼(Servlet/JSP) »èÁ¦µÊ
+		list.remove("iBATIS");
+		
+		for(int i = 0; i<list.size(); i++) {
+			String str = list.get(i);
+			System.out.println(i + ":" + str);
+		}
+		
+}
+}
